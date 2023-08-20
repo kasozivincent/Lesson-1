4677b30e50cab75b86b996b138f022f6d97128c7
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-08-20 16:08:54 +0100

    Loops

---
+++ utilities.py
@@ -0,0 +1,19 @@
+def is_even(number: int) -> bool:
+    if(number % 2 == 0):
+        return True
+    else:
+        return False
+    
+
+def is_one_digit(number: int) -> bool:
+    return number < 10
+
+
+def is_odd(number: int) -> bool:
+    if (number % 2 == 1):
+        return  True
+    else:
+        return False
+
+def get_last_digit(number: int) -> int:
+    return number % 10
