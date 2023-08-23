c04a5ba0d00ee897f79f136d0e759f7a383615c8
Kasozi Vincent <kasozivincentug507@gmail.com>
2023-08-23 18:12:31 +0100

    Lesson13

--- utilities.py
+++ utilities.py
@@ -1,9 +1,9 @@
 def is_even(number: int) -> bool:
-    if(number % 2 == 0):
+    if (number % 2 == 0):
         return True
     else:
         return False
-    
+
 
 def is_one_digit(number: int) -> bool:
     return number < 10
@@ -11,12 +11,22 @@ def is_one_digit(number: int) -> bool:
 
 def is_odd(number: int) -> bool:
     if (number % 2 == 1):
-        return  True
+        return True
     else:
         return False
+
 
 def get_last_digit(number: int) -> int:
     return number % 10
 
+
 def max(a, b):
     return a if a >= b else b
+
+
+def number_of_rows(matrix) -> int:
+    return len(matrix)
+
+
+def columns(matrix):
+    return len(matrix[0])
